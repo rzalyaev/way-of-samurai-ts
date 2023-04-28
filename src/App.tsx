1b306b95989ed3e294f5c1a32bf8eb5eea1c9137
rzalyaev <ruslan.zalyaev@gmail.com>
2023-04-29 02:25:39 +0400

    Delete Technologies component. Add Navbar and Header components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
-import Header from "./Header";
-import Technologies from "./Technologies";
+import Header from "./components/Header/Header";
+import { Navbar } from "./components/Navbar/Navbar";
+import { Content } from './components/Content/Content';
 
-function App() {
+const App = () => {
     return (
         <div className="App">
             <Header/>
-            <Technologies/>
+            <Navbar/>
+            <Content/>
         </div>
     );
 }
