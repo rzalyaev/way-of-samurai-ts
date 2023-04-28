b80b3d338c6e35c62b28a22ea9946f253024de34
rzalyaev <ruslan.zalyaev@gmail.com>
2023-04-29 02:25:05 +0400

    Change Header component content

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./Header.module.css";
+
+const Header = () => {
+    return (
+        <div className={styles.header}>
+            <img src="#" alt="logo"/>
+        </div>
+    )
+}
+
+export default Header;
