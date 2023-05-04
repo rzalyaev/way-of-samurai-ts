e1b4495d08c68419481ec9dc45278e2b760a0fd5
rzalyaev <ruslan.zalyaev@gmail.com>
2023-05-04 23:30:01 +0400

    Add BrowserRouter wrapper due to Routes addition

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,17 @@ import './App.css';
 import Header from "./components/Header/Header";
 import { Navbar } from "./components/Navbar/Navbar";
 import { Content } from './components/Content/Content';
+import {BrowserRouter} from "react-router-dom";
 
 const App = () => {
     return (
-        <div className="App">
-            <Header/>
-            <Navbar/>
-            <Content/>
-        </div>
+        <BrowserRouter>
+            <div className="App">
+                <Header/>
+                <Navbar/>
+                <Content/>
+            </div>
+        </BrowserRouter>
     );
 }
 
