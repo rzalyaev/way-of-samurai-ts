b136cf219094636dfd770938d0ba9c74bfb430f3
rzalyaev <ruslan.zalyaev@gmail.com>
2023-05-04 23:28:36 +0400

    Delete anchor tags, add NavLink instead

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,15 +1,26 @@
 import React from "react";
 import styles from "./Navbar.module.css";
+import {NavLink} from "react-router-dom";
 
 export const Navbar = () => {
     return(
         <div className={styles.navbar}>
             <ul className={styles.navbar__list}>
-                <li className={styles.navbar__item}><a href="#">Profile</a></li>
-                <li className={styles.navbar__item}><a href="#">Messages</a></li>
-                <li className={styles.navbar__item}><a href="#">News</a></li>
-                <li className={styles.navbar__item}><a href="#">Music</a></li>
-                <li className={styles.navbar__item}><a href="#">Settings</a></li>
+                <li className={styles.navbar__item}>
+                    <NavLink to="/profile" activeClassName={styles.active}>Profile</NavLink>
+                </li>
+                <li className={styles.navbar__item}>
+                    <NavLink to="/messenger" activeClassName={styles.active}>Messages</NavLink>
+                </li>
+                <li className={styles.navbar__item}>
+                    <NavLink to="/feed" activeClassName={styles.active}>News</NavLink>
+                </li>
+                <li className={styles.navbar__item}>
+                    <NavLink to="/music" activeClassName={styles.active}>Music</NavLink>
+                </li>
+                <li className={styles.navbar__item}>
+                    <NavLink to="/settings" activeClassName={styles.active}>Settings</NavLink>
+                </li>
             </ul>
         </div>
     )
