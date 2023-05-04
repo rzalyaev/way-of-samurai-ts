c9a778c104d8696c1bb15d15af4dae57a7c67ae4
rzalyaev <ruslan.zalyaev@gmail.com>
2023-05-04 23:29:31 +0400

    Add Messenger, News, Music and Settings components. Add Routes

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -1,11 +1,20 @@
 import React from "react";
 import styles from "./Content.module.css";
-import { Profile } from "./Profile/Profile";
+import {Route} from "react-router-dom";
+import {Profile} from "./Profile/Profile";
+import {Messenger} from "./Messenger/Messenger";
+import {News} from "./News/News";
+import {Music} from "./Music/Music";
+import {Settings} from "./Settings/Settings";
 
 export const Content = () => {
     return(
         <div className={styles.content}>
-            <Profile/>
+            <Route path={'/profile'} component={Profile}/>
+            <Route path={'/messenger'} component={Messenger}/>
+            <Route path={'/feed'} component={News}/>
+            <Route path={'/music'} component={Music}/>
+            <Route path={'/settings'} component={Settings}/>
         </div>
     )
 }
