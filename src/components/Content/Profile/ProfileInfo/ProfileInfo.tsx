241cef3961b4ec5af04aa506d8156f272c6b63ff
rzalyaev <ruslan.zalyaev@gmail.com>
2023-05-08 01:30:13 +0400

    Add types. Add data from state

--- src/components/Content/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Content/Profile/ProfileInfo/ProfileInfo.tsx
@@ -1,16 +1,21 @@
 import React from "react";
 import styles from "./ProfileInfo.module.css";
+import {ProfileInfoType} from "../../../../redux/state";
 
-export const ProfileInfo = () => {
+type PropsType = {
+    info: ProfileInfoType
+}
+
+export const ProfileInfo: React.FC<PropsType> = ({info}) => {
     return(
         <div>
             <h3>Profile Info Component</h3>
             <img src="#" alt="user avatar"/>
             <div>
                 <ul>
-                    <li>Name: Ruslan</li>
-                    <li>Status: I love coding!</li>
-                    <li>City: Izhevsk</li>
+                    <li>Name: {info.name}</li>
+                    <li>Status: {info.status}</li>
+                    <li>City: {info.location.city}</li>
                 </ul>
             </div>
         </div>
