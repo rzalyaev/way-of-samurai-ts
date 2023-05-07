2c9bd022f6c6835d3f82fab877952536060f8c70
rzalyaev <ruslan.zalyaev@gmail.com>
2023-05-08 01:31:19 +0400

    Add types. Add data from props to child components

--- src/components/Content/Profile/Profile.tsx
+++ src/components/Content/Profile/Profile.tsx
@@ -1,15 +1,21 @@
 import React from "react";
 import styles from "./Profile.module.css";
-import { ProfileInfo } from "./ProfileInfo/ProfileInfo";
-import { SendPost } from "./SendPost/SendPost";
-import { PostsList } from "./PostsList/PostsList";
+import {ProfileInfo} from "./ProfileInfo/ProfileInfo";
+import {SendPost} from "./SendPost/SendPost";
+import {PostsList} from "./PostsList/PostsList";
+import {ProfileInfoType, PostType} from "../../../redux/state";
 
-export const Profile = () => {
+type PropsType = {
+    info: ProfileInfoType
+    posts: PostType[]
+}
+
+export const Profile: React.FC<PropsType> = ({info, posts}) => {
     return(
         <div>
-            <ProfileInfo/>
+            <ProfileInfo info={info}/>
             <SendPost/>
-            <PostsList/>
+            <PostsList posts={posts}/>
         </div>
     )
 }
