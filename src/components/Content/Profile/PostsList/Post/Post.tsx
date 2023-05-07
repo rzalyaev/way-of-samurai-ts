45b93a9dd726e55bfd3aca00cad40bd73f0d790d
rzalyaev <ruslan.zalyaev@gmail.com>
2023-05-08 01:32:28 +0400

    Add types. Add data from props

--- src/components/Content/Profile/PostsList/Post/Post.tsx
+++ src/components/Content/Profile/PostsList/Post/Post.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import styles from "./Post.module.css";
+import {PostType} from "../../../../../redux/state";
 
-export const Post = () => {
+export const Post: React.FC<PostType> = ({text, likes}) => {
     return(
         <div className={styles.post}>
             <img src="#" alt="user avatar small"/>
-            <div>Here will be post text</div>
-            <button>Likes: 0</button>
+            <div>{text}</div>
+            <button>Likes: {likes}</button>
         </div>
     )
 }
