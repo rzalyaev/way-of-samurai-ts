af0926cbf719b22d2b89bd83e7dbeed1f55aa893
rzalyaev <ruslan.zalyaev@gmail.com>
2023-05-08 01:33:35 +0400

    Change MessageType import way

--- src/components/Content/Messenger/MessagesBlock/MessagesBlock.tsx
+++ src/components/Content/Messenger/MessagesBlock/MessagesBlock.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import styles from "./MessagesBlock.module.css";
 import Messages from "./Messages/Messages";
-import {MessageType} from "../Messenger";
 import SendMessageForm from "./SendMessageForm/SendMessageForm";
+import {MessageType} from "../../../../redux/state";
+
 
 type PropsType = {
     messages: MessageType[]
