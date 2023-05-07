e49bb109f024b77d131ebc626b2d5c6258a73649
rzalyaev <ruslan.zalyaev@gmail.com>
2023-05-08 01:34:37 +0400

    Add types. Change component to render in Routes

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -6,12 +6,22 @@ import {Messenger} from "./Messenger/Messenger";
 import {News} from "./News/News";
 import {Music} from "./Music/Music";
 import {Settings} from "./Settings/Settings";
+import {MessengerPageType, ProfilePageType} from "../../redux/state";
 
-export const Content = () => {
+type PropsType = {
+    profile: ProfilePageType
+    messenger: MessengerPageType
+}
+
+export const Content: React.FC<PropsType> = ({profile, messenger}) => {
     return(
         <div className={styles.content}>
-            <Route path={'/profile'} component={Profile}/>
-            <Route path={'/messenger'} component={Messenger}/>
+            <Route path={'/profile'}
+                   render={() => <Profile info={profile.profileInfo} posts={profile.posts}/>}
+            />
+            <Route path={'/messenger'}
+                   render={() => <Messenger contacts={messenger.contacts} messages={messenger.messages}/>}
+            />
             <Route path={'/feed'} component={News}/>
             <Route path={'/music'} component={Music}/>
             <Route path={'/settings'} component={Settings}/>
