11ee13051b857e6a4e6a52265419d5870eaacc30
rzalyaev <ruslan.zalyaev@gmail.com>
2023-05-11 23:57:48 +0400

    Add opportunity to add post

--- src/components/Content/Profile/SendPost/SendPost.tsx
+++ src/components/Content/Profile/SendPost/SendPost.tsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, {ChangeEvent, useState} from "react";
 import styles from "./SendPost.module.css";
 
-export const SendPost = () => {
+type PropsType = {
+    addPost: (newPostText: string) => void
+}
+
+export const SendPost: React.FC<PropsType> = ({addPost}) => {
+    const [postText, setPostText] = useState<string>('');
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setPostText(e.currentTarget.value);
+    }
+
+    const onClickHandler = () => {
+        addPost(postText);
+        setPostText('');
+    }
+
     return(
         <div>
             <h3>Send Post Component</h3>
-            <input type="textarea"/>
-            <button>Send Post</button>
+            <input type="textarea" value={postText} onChange={onChangeHandler}/>
+            <button onClick={onClickHandler}>Send Post</button>
         </div>
     )
 }
