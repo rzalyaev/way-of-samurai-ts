b562674b08dd98d42099f843e9cc57e228148073
rzalyaev <ruslan.zalyaev@gmail.com>
2023-05-15 23:31:52 +0400

    Add send message functionality

--- src/components/Content/Messenger/MessagesBlock/SendMessageForm/SendMessageForm.tsx
+++ src/components/Content/Messenger/MessagesBlock/SendMessageForm/SendMessageForm.tsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, {ChangeEvent, FC, useState} from 'react';
 import styles from "./SendMessageForm.module.css";
+import {ActionsTypes, sendMessageAC} from "../../../../../redux/state";
 
-const SendMessageForm = () => {
+type PropsType = {
+    dispatch: (action: ActionsTypes) => void,
+}
+
+const SendMessageForm: FC<PropsType> = ({dispatch}) => {
+    const [messageText, setMessageText] = useState<string>('');
+
+    const handleOnChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setMessageText(e.currentTarget.value);
+    }
+
+    const handleOnClick = () => {
+        dispatch(sendMessageAC(messageText));
+        setMessageText('');
+    }
+
     return (
         <div className={styles.sendMessageForm}>
-            <input type="text"/>
-            <button>Send</button>
+            <input type="text" value={messageText} onChange={handleOnChange}/>
+            <button onClick={handleOnClick}>Send</button>
         </div>
     );
 };
