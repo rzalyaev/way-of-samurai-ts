8209435ef2453b688fbc06281c9d4f4ca7b700c1
rzalyaev <ruslan.zalyaev@gmail.com>
2023-05-15 23:26:17 +0400

    Add dispatch to PropsType, Profile and Messenger

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -6,27 +6,27 @@ import {Messenger} from "./Messenger/Messenger";
 import {News} from "./News/News";
 import {Music} from "./Music/Music";
 import {Settings} from "./Settings/Settings";
-import {MessengerPageType, ProfilePageType} from "../../redux/state";
+import {ActionsTypes, MessengerPageType, ProfilePageType} from "../../redux/state";
 
 type PropsType = {
     profile: ProfilePageType
     messenger: MessengerPageType
-    addPost: (newPostText: string) => void
+    dispatch: (action: ActionsTypes) => void
 }
 
 export const Content: React.FC<PropsType> = (
     {
         profile,
         messenger,
-        addPost,
+        dispatch,
     }) => {
     return(
         <div className={styles.content}>
             <Route path={'/profile'}
-                   render={() => <Profile info={profile.profileInfo} posts={profile.posts} addPost={addPost}/>}
+                   render={() => <Profile info={profile.profileInfo} posts={profile.posts} dispatch={dispatch}/>}
             />
             <Route path={'/messenger'}
-                   render={() => <Messenger contacts={messenger.contacts} messages={messenger.messages}/>}
+                   render={() => <Messenger contacts={messenger.contacts} messages={messenger.messages} dispatch={dispatch}/>}
             />
             <Route path={'/feed'} component={News}/>
             <Route path={'/music'} component={Music}/>
