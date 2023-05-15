4fbc6450a391244f77b6d4afcec78abfc01cc8af
rzalyaev <ruslan.zalyaev@gmail.com>
2023-05-15 23:30:12 +0400

    Add dispatch to PropsType and SendPost props

--- src/components/Content/Profile/Profile.tsx
+++ src/components/Content/Profile/Profile.tsx
@@ -3,24 +3,24 @@ import styles from "./Profile.module.css";
 import {ProfileInfo} from "./ProfileInfo/ProfileInfo";
 import {SendPost} from "./SendPost/SendPost";
 import {PostsList} from "./PostsList/PostsList";
-import {ProfileInfoType, PostType} from "../../../redux/state";
+import {ProfileInfoType, PostType, ActionsTypes} from "../../../redux/state";
 
 type PropsType = {
     info: ProfileInfoType
     posts: PostType[]
-    addPost: (newPostText: string) => void
+    dispatch: (action: ActionsTypes) => void
 }
 
 export const Profile: React.FC<PropsType> = (
     {
         info,
         posts,
-        addPost,
+        dispatch,
     }) => {
     return(
         <div>
             <ProfileInfo info={info}/>
-            <SendPost addPost={addPost}/>
+            <SendPost dispatch={dispatch}/>
             <PostsList posts={posts}/>
         </div>
     )
