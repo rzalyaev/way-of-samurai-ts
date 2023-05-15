4aa6fa4be65eb74e134ed48d38cf2948a58e3c2c
rzalyaev <ruslan.zalyaev@gmail.com>
2023-05-15 23:29:31 +0400

    Add dispatch to PropsType and MessageBlock props

--- src/components/Content/Messenger/Messenger.tsx
+++ src/components/Content/Messenger/Messenger.tsx
@@ -2,18 +2,19 @@ import React from "react";
 import styles from "./Messenger.module.css";
 import Contacts from "./Contacts/Contacts";
 import MessagesBlock from "./MessagesBlock/MessagesBlock";
-import {ContactType, MessageType} from "../../../redux/state";
+import {ActionsTypes, ContactType, MessageType} from "../../../redux/state";
 
 type PropsType = {
-    contacts: ContactType[]
-    messages: MessageType[]
+    contacts: ContactType[],
+    messages: MessageType[],
+    dispatch: (action: ActionsTypes) => void,
 }
 
-export const Messenger: React.FC<PropsType> = ({contacts, messages}) => {
-    return(
+export const Messenger: React.FC<PropsType> = ({contacts, messages, dispatch}) => {
+    return (
         <div className={styles.messenger}>
             <Contacts contacts={contacts}/>
-            <MessagesBlock messages={messages}/>
+            <MessagesBlock messages={messages} dispatch={dispatch}/>
         </div>
     )
 }
