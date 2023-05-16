d52d9432c6b277b6e8cd6b9391d7586418638b26
rzalyaev <ruslan.zalyaev@gmail.com>
2023-05-17 02:51:59 +0400

    Delete contacts and messages from props, add messenger instead

--- src/components/Content/Messenger/Messenger.tsx
+++ src/components/Content/Messenger/Messenger.tsx
@@ -2,19 +2,18 @@ import React from "react";
 import styles from "./Messenger.module.css";
 import Contacts from "./Contacts/Contacts";
 import MessagesBlock from "./MessagesBlock/MessagesBlock";
-import {ActionsTypes, ContactType, MessageType} from "../../../redux/store";
+import {ActionsTypes, MessengerPageType} from "../../../redux/store";
 
 type PropsType = {
-    contacts: ContactType[],
-    messages: MessageType[],
+    messenger: MessengerPageType,
     dispatch: (action: ActionsTypes) => void,
 }
 
-export const Messenger: React.FC<PropsType> = ({contacts, messages, dispatch}) => {
+export const Messenger: React.FC<PropsType> = ({messenger, dispatch}) => {
     return (
         <div className={styles.messenger}>
-            <Contacts contacts={contacts}/>
-            <MessagesBlock messages={messages} dispatch={dispatch}/>
+            <Contacts contacts={messenger.contacts}/>
+            <MessagesBlock messages={messenger.messages} dispatch={dispatch}/>
         </div>
     )
 }
