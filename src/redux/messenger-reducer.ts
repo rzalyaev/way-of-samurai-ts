57e2f4a7f00213eb243a3423103651aecfce93d3
rzalyaev <ruslan.zalyaev@gmail.com>
2023-06-03 23:35:04 +0400

    Add initial state

--- src/redux/messenger-reducer.ts
+++ src/redux/messenger-reducer.ts
@@ -2,7 +2,22 @@ import {ActionsTypes, MessengerPageType} from "./store";
 
 const SEND_MESSAGE = 'SEND_MESSAGE';
 
-export const messengerReducer = (state: MessengerPageType, action: ActionsTypes) => {
+const initialState = {
+    contacts: [
+        {id: 1, name: 'John'},
+        {id: 2, name: 'Jenifer'},
+        {id: 3, name: 'Brian'},
+        {id: 4, name: 'Paul'},
+        {id: 5, name: 'Rick'},
+    ],
+    messages: [
+        {id: 1, text: 'Hello!', time: '09:03'},
+        {id: 2, text: 'How are you?', time: '09:05'},
+        {id: 3, text: `I'm fine!`, time: '09:07'},
+    ]
+}
+
+export const messengerReducer = (state: MessengerPageType = initialState, action: ActionsTypes) => {
     switch (action.type) {
         case SEND_MESSAGE:
             let newMessage = {
