9851a43b8fe4e65216a29d7c12cc6e3d53fd1bd3
rzalyaev <ruslan.zalyaev@gmail.com>
2023-06-07 02:08:39 +0400

    Remove dispatch from props

--- src/components/Content/Profile/Profile.tsx
+++ src/components/Content/Profile/Profile.tsx
@@ -1,20 +1,19 @@
 import React from 'react';
 import styles from './Profile.module.css';
-import {ActionsTypes, ProfilePageType} from '../../../redux/store';
+import {ProfilePageType} from '../../../redux/store';
 import ProfileInfo from './ProfileInfo/ProfileInfo';
 import SendPostContainer from './SendPost/SendPostContainer';
 import PostsList from './PostsList/PostsList';
 
 type PropsType = {
-    profile: ProfilePageType,
-    dispatch: (action: ActionsTypes) => void
+    profile: ProfilePageType
 }
 
-const Profile = ({profile, dispatch}: PropsType) => {
+const Profile = ({profile}: PropsType) => {
     return(
         <div>
             <ProfileInfo info={profile.profileInfo}/>
-            <SendPostContainer dispatch={dispatch}/>
+            <SendPostContainer/>
             <PostsList posts={profile.posts}/>
         </div>
     );
