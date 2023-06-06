9f7c7fe2245c3796d646f144658782f4b8bc5f83
rzalyaev <ruslan.zalyaev@gmail.com>
2023-06-07 02:08:20 +0400

    Remove dispatch from props

--- src/components/Content/Messenger/MessagesBlock/MessagesBlock.tsx
+++ src/components/Content/Messenger/MessagesBlock/MessagesBlock.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
 import styles from './MessagesBlock.module.css';
-import {ActionsTypes, MessageType} from '../../../../redux/store';
+import {MessageType} from '../../../../redux/store';
 import Messages from './Messages/Messages';
 import SendMessageFormContainer from './SendMessageForm/SendMessageFormContainer';
 
 type PropsType = {
-    messages: MessageType[],
-    dispatch: (action: ActionsTypes) => void,
+    messages: MessageType[]
 }
 
-const MessagesBlock = ({messages, dispatch}: PropsType) => {
+const MessagesBlock = ({messages}: PropsType) => {
     return (
         <div className={styles.messagesBlock}>
             <Messages messages={messages}/>
-            <SendMessageFormContainer dispatch={dispatch}/>
+            <SendMessageFormContainer/>
         </div>
     );
 };
