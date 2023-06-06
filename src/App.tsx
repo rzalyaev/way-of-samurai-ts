bcf5ecbf301b393e01f03136c24f49631f77a300
rzalyaev <ruslan.zalyaev@gmail.com>
2023-06-07 02:08:01 +0400

    Remove props

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,13 @@ import './App.css';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Content from './components/Content/Content';
-import {ActionsTypes, RootStateType} from './redux/store';
 
-type PropsType = {
-    state: RootStateType,
-    dispatch: (action: ActionsTypes) => void
-}
-
-const App = ({state, dispatch}: PropsType) => {
+const App = () => {
     return (
         <div className="App">
             <Header/>
             <Navbar/>
-            <Content state={state} dispatch={dispatch}/>
+            <Content/>
         </div>
     );
 }
