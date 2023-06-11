e7715e560540cc0e7f02bffb0bba73c99f901efb
rzalyaev <ruslan.zalyaev@gmail.com>
2023-06-11 18:30:37 +0400

    Remove state as an argument due to redux addition

--- src/index.tsx
+++ src/index.tsx
@@ -3,17 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import store from './redux/redux-store';
-import {RootStateType} from "./redux/store";
 import {BrowserRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 
-const rerenderEntireTree = (state: RootStateType) => ReactDOM.render(
+const rerenderEntireTree = () => ReactDOM.render(
     <BrowserRouter>
         <Provider store={store}>
                 <App/>
         </Provider>
     </BrowserRouter>, document.getElementById('root'));
 
-rerenderEntireTree(store.getState());
+rerenderEntireTree();
 
-store.subscribe(() => rerenderEntireTree(store.getState()));
+store.subscribe(() => rerenderEntireTree());
