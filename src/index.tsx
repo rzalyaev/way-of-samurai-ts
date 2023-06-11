cb739e04862732b9dc863ba47838942e356ad597
rzalyaev <ruslan.zalyaev@gmail.com>
2023-06-11 18:29:51 +0400

    Remove context due to redux addition

--- src/index.tsx
+++ src/index.tsx
@@ -5,16 +5,13 @@ import App from './App';
 import store from './redux/redux-store';
 import {RootStateType} from "./redux/store";
 import {BrowserRouter} from "react-router-dom";
-import {StoreContext} from "./redux/redux-store";
-import {StoreDispatchContext} from "./redux/redux-store";
+import {Provider} from "react-redux";
 
 const rerenderEntireTree = (state: RootStateType) => ReactDOM.render(
     <BrowserRouter>
-        <StoreContext.Provider value={state}>
-            <StoreDispatchContext.Provider value={store.dispatch.bind(store)}>
+        <Provider store={store}>
                 <App/>
-            </StoreDispatchContext.Provider>
-        </StoreContext.Provider>
+        </Provider>
     </BrowserRouter>, document.getElementById('root'));
 
 rerenderEntireTree(store.getState());
