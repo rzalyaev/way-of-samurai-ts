2cfe370a4a438a238db86d9dc7530a68c1306f6d
rzalyaev <ruslan.zalyaev@gmail.com>
2023-06-11 18:34:46 +0400

    Get messengerPage from state via useSelector

--- src/components/Content/Messenger/Messenger.tsx
+++ src/components/Content/Messenger/Messenger.tsx
@@ -2,13 +2,12 @@ import React from 'react';
 import styles from './Messenger.module.css';
 import Contacts from './Contacts/Contacts';
 import MessagesBlock from './MessagesBlock/MessagesBlock';
-import {MessengerPageType} from '../../../redux/store';
+import {useSelector} from "react-redux";
+import {RootStateType} from "../../../redux/store";
 
-type PropsType = {
-    messenger: MessengerPageType
-}
+const Messenger = () => {
+    const messenger = useSelector((state: RootStateType) => state.messengerPage);
 
-const Messenger = ({messenger}: PropsType) => {
     return (
         <div className={styles.messenger}>
             <Contacts contacts={messenger.contacts}/>
